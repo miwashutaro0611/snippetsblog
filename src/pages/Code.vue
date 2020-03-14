5ef4314099ed235a18a8769a3b6bc5d533048549
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-03-14 23:31:04 +0900

    TOPへのリンクボタン追加

--- src/pages/Code.vue
+++ src/pages/Code.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1 class="codeTitle">{{ snippets[$route.params.id - 1].title }}</h1>
-    <p>{{ snippets[$route.params.id - 1].id }}</p>
     <div class="codeWrap">
       <component :is="snippets[$route.params.id - 1].link" />
     </div>
@@ -10,6 +9,9 @@
       v-if="snippets[$route.params.id - 1].codepen"
       v-html="snippets[$route.params.id - 1].codepen"
     />
+    <router-link to="/" class="topLinkBtn">
+      GO TO LIST
+    </router-link>
   </div>
 </template>
 
@@ -40,5 +42,46 @@ export default {
 }
 .codePen {
   margin-top: 20px;
+}
+.topLinkBtn {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 300px;
+  width: calc(100% - 20px);
+  height: 60px;
+  margin: 40px auto 0;
+  text-align: center;
+  border: 2px solid var(--color-default);
+  border-radius: 30px;
+  letter-spacing: 0.1em;
+  transition: all 0.2s ease-in-out;
+  overflow: hidden;
+  @include sm {
+    margin-top: 40px;
+  }
+  &::before {
+    content: '';
+    background-color: var(--color-btn-mask);
+    height: 100%;
+    width: 3em;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: -4.5em;
+    transform: skewX(-45deg) translateX(0);
+    transition: none;
+  }
+  @media (hover: hover) {
+    &:hover {
+      background-color: var(--color-default);
+      color: var(--color-default-reverse);
+      &::before {
+        transform: skewX(-45deg) translateX(400px);
+        transition: all 0.5s ease-in-out;
+      }
+    }
+  }
 }
 </style>
