3a37c0920d03cb51b772ff2de5e4a4ea9dba84ad
miwashutaro0611 <jacksumiwa0061@gmail.com>
2021-11-16 10:54:46 +0900

    feat: 詳細画面のコンポーネントのiframe化化

---
+++ src/pages/iframe/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="snippets[$route.params.id - 1].link" />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'Iframe',
+  layout: 'iframe',
+  computed: {
+    ...mapState({
+      snippets: 'snippets',
+    }),
+  },
+  head() {
+    return {
+      title: this.snippets[this.$route.params.id - 1].title,
+    }
+  },
+}
+</script>
